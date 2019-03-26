7122bd9f9eaa66e5ce00bbed66190b5f5b83fe19
Edvard Sagsletten <edvardsa@stud.ntnu.no>
2019-03-26 13:17:55 +0100

    updated cost function

--- Tester/test.go
+++ Tester/test.go
@@ -1,48 +1,50 @@
 package main
 
 import (
-	. "./cost"
-	. "./orders/ordStruct"
-	. "./orders/ordStruct/elevio"
+	"../cost"
+	"../orders/elevio/ordStruct"
+	"../MakkerModul/decoding"
 	"fmt"
-	"time"
 )
 
 func main() {
-	elevator_1 := Elevator{
-		ID:        1,
+	elevator_1 := ordStruct.Elevator{
+		ID:        "1",
 		Floor:     0,
 		NumFloors: 4,
-		Dir:       MD_Up,
+		Dir:       ordStruct.MD_Up,
 		Order: [3][4]int{
 			{0, 0, 0, 0},
 			{0, 0, 0, 0},
 			{0, 0, 0, 0},
 		},
-		OpenDoorTime: 3 * time.Second,
-		Behaviour:    E_Idle,
+		Behaviour:    ordStruct.E_Idle,
 	}
 
-	elevator_2 := Elevator{
-		ID:        2,
+	elevator_status_1 := decoding.ElevatorStatus{E: elevator_1}
+
+	elevator_2 := ordStruct.Elevator{
+		ID:        "2",
 		Floor:     1,
 		NumFloors: 4,
-		Dir:       MD_Down,
+		Dir:       ordStruct.MD_Down,
 		Order: [3][4]int{
 			{0, 0, 0, 0},
 			{0, 0, 0, 0},
 			{1, 0, 0, 0},
 		},
-		OpenDoorTime: 3 * time.Second,
-		Behaviour:    E_Moving,
+		Behaviour:    ordStruct.E_Moving,
 	}
-	var elevators []Elevator
-	elevators = append(elevators, elevator_1)
-	elevators = append(elevators, elevator_1)
 
-	time_Elevator_1 := TimeToServeRequest(elevator_1, 1, 3)
-	time_Elevator_2 := TimeToServeRequest(elevator_2, 1, 3)
-	identification := ChooseElevator(elevators, 1, 3)
+	elevator_status_2 := decoding.ElevatorStatus{E: elevator_2}
+
+	elevators:= make(map[string]decoding.ElevatorStatus)
+	elevators["1"] = elevator_status_1
+	elevators["2"] = elevator_status_2
+
+	time_Elevator_1 := cost.TimeToServeRequest(elevator_status_1.E, 1, 3)
+	time_Elevator_2 := cost.TimeToServeRequest(elevator_status_2.E, 1, 3)
+	identification := cost.ChooseElevator(elevators, 1, 3)
 	fmt.Println("Elevator 1 uses: ", time_Elevator_1)
 	fmt.Println("Elevator 2 uses: ", time_Elevator_2)
 	fmt.Println("We choose elevator: ", identification, " for the order")
--- cost/cost.go
+++ cost/cost.go
@@ -1,44 +1,46 @@
 package cost
 
 import (
-
-	."../orders"
-	."../orders/elevio"
-	."../orders/elevio/ordStruct"
+	"../orders"
+	//."../orders/elevio"
+	"../orders/elevio/ordStruct"
+    "../MakkerModul/decoding"
 )
 
+//constants for doing the calculations in TimeToServeRequest
+//not to be used anywhere else
 const TRAVEL_TIME = 2
 const DOOR_OPEN_TIME = 3
 
 
-func TimeToServeRequest(e_old Elevator, button ButtonType, floor int) int {
+func TimeToServeRequest(e_old ordStruct.Elevator, button ordStruct.ButtonType, floor int) int {
     e := e_old
     e.Order[button][floor] = 1
     duration := 0
     
     switch e.Behaviour{
-    case E_Idle:
-        e.Dir = ChooseDirection(e)
-        if(e.Dir == MD_Stop){
+    case ordStruct.E_Idle:
+        e.Dir = orders.ChooseDirection(e)
+        if(e.Dir == ordStruct.MD_Stop){
             return duration
         }
         break
-    case E_Moving:
+    case ordStruct.E_Moving:
         duration += TRAVEL_TIME/2;
         e.Floor += int(e.Dir)
         break
-    case E_DoorOpen:
+    case ordStruct.E_DoorOpen:
         duration -= DOOR_OPEN_TIME/2
     }
 
     for {
-        if(ShouldStop(e) == true){
+        if(orders.ShouldStop(e) == true){
             e = ClearOrdersAtCurrentFloor_Cost(e)
             if(e.Floor == floor){
                 return duration
             }
             duration += DOOR_OPEN_TIME
-            e.Dir = ChooseDirection(e)
+            e.Dir = orders.ChooseDirection(e)
         }
         e.Floor += int(e.Dir)
         duration += TRAVEL_TIME
@@ -46,7 +48,8 @@ func TimeToServeRequest(e_old Elevator, button ButtonType, floor int) int {
 }
 
 
-func ClearOrdersAtCurrentFloor_Cost(e Elevator) Elevator {
+//simulated clear order function to be sure to not mess with actual elevator states
+func ClearOrdersAtCurrentFloor_Cost(e ordStruct.Elevator) ordStruct.Elevator {
 	e2 := e.Duplicate()
 	for btn := 0; btn < 3; btn++ {
 		if e2.Order[btn][e2.Floor] != 0 {
@@ -57,15 +60,17 @@ func ClearOrdersAtCurrentFloor_Cost(e Elevator) Elevator {
 }
 
 
-func ChooseElevator(elevators []Elevator, button ButtonType, floor int) int {
+func ChooseElevator(elevators map[string]decoding.ElevatorStatus, button ordStruct.ButtonType, floor int) string {
 	minimum := 0
-	var elevator_id int 
-	for i,elevator := range elevators{
-		temp :=  TimeToServeRequest(elevator, button, floor)
+    i := 0 // use i to tell us if it's the first time iterating
+	var elevator_id string 
+	for elevator_ID,elevator_status := range elevators{
+		temp :=  TimeToServeRequest(elevator_status.E, button, floor)
 		if i == 0 || temp < minimum {
                 minimum = temp
-				elevator_id = elevator.ID 
+				elevator_id = elevator_ID
 		}
+        i++
 	}
 	return elevator_id
 }
