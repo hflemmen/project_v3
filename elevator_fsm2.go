ac517729b01cf783ea453ad4db9f5f5db95cf43d
JOhan Gangsås Hole <johangh@stud.ntnu.no>
2019-03-26 22:29:11 +0100

    Added elevator_fsm2

---
+++ elevator_fsm2.go
@@ -0,0 +1,208 @@
+package main
+
+import (
+	"./MakkerModul/connector"
+	"./MakkerModul/decoding"
+	"./orders"
+	"./orders/elevio"
+	"./orders/elevio/ordStruct"
+	"flag"
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Starting elevator")
+	floors := flag.Int("floors", 4, "number of floors")
+	port := flag.Int("port", 15658, "number of floors")
+	flag.Parse()
+	numFloors := *floors
+
+	if numFloors == 0 {
+		numFloors = 4
+	}
+	if *port == 0 {
+		*port = 15657
+	}
+
+	elevio.Init(fmt.Sprintf("localhost:%v", *port), numFloors)
+	e := ordStruct.ElevatorInit("elev", numFloors)
+	states := make(chan ordStruct.Elevator)
+	//elevio.SetMotorDirection(d)
+
+	newButton := make(chan ordStruct.ButtonEvent)
+	newOrders := make(chan ordStruct.ButtonEvent)
+	floorArrivals := make(chan int)
+	updateLights := make(chan ordStruct.LightType)
+	receiveLocal, msgChanLocal := connector.EstablishLocalTunnel(
+		"msgHandler2.go", 44445, 55556)
+	go elevio.PollButtons(newButton, newOrders)
+	go elevio.PollFloorSensor(floorArrivals)
+	go connectionMsgHandler(receiveLocal, msgChanLocal, newOrders, states, updateLights)
+	elevator_fsm(e, newOrders, floorArrivals, states, updateLights, newButton)
+}
+
+func elevator_fsm(e ordStruct.Elevator, newOrders <-chan ordStruct.ButtonEvent,
+	floorArrivals <-chan int, states chan ordStruct.Elevator,
+	updateLights <-chan ordStruct.LightType, newButton <-chan ordStruct.ButtonEvent) {
+
+	var doorTimer <-chan time.Time
+	f := elevio.GetFloor()
+	if f == -1 {
+		elevio.SetMotorDirection(ordStruct.MD_Down)
+		e.Behaviour = ordStruct.E_Moving
+	} else {
+		elevio.SetMotorDirection(ordStruct.MD_Stop)
+		doorTimer = time.After(ordStruct.DOOR_OPEN_TIME)
+		e.Floor = f
+		e.Behaviour = ordStruct.E_DoorOpen
+	}
+	for {
+		prevElevator := e
+	sel:
+		select {
+		case a := <-newButton:
+			switch e.Behaviour {
+			case ordStruct.E_Idle:
+				if e.Floor == a.Floor {
+					elevio.SetDoorOpenLamp(true)
+					doorTimer = time.After(ordStruct.DOOR_OPEN_TIME)
+					e.Behaviour = ordStruct.E_DoorOpen
+				} else {
+					if a.Button == ordStruct.BT_Cab {
+						e.Order[a.Button][a.Floor] = true
+					} else {
+						e.LightMatrix[a.Button][a.Floor] = true
+					}
+				}
+
+			case ordStruct.E_Moving:
+				if a.Button == ordStruct.BT_Cab {
+					e.Order[a.Button][a.Floor] = true
+				} else {
+					e.LightMatrix[a.Button][a.Floor] = true
+				}
+
+			case ordStruct.E_DoorOpen:
+				if e.Floor == a.Floor {
+					doorTimer = time.After(ordStruct.DOOR_OPEN_TIME)
+				} else {
+					if a.Button == ordStruct.BT_Cab {
+						e.Order[a.Button][a.Floor] = true
+					} else {
+						e.LightMatrix[a.Button][a.Floor] = true
+					}
+				}
+			}
+			if e != prevElevator {
+				states <- e.Duplicate()
+			}
+		case a := <-newOrders:
+			switch e.Behaviour {
+			case ordStruct.E_Idle:
+				if e.Floor == a.Floor {
+					elevio.SetDoorOpenLamp(true)
+					doorTimer = time.After(ordStruct.DOOR_OPEN_TIME)
+					e.Behaviour = ordStruct.E_DoorOpen
+				} else {
+					e.Order[a.Button][a.Floor] = true
+					if a.Button == ordStruct.BT_Cab {
+						elevio.SetButtonLamp(ordStruct.BT_Cab, a.Floor, true)
+					}
+					e.Dir = orders.ChooseDirection(e)
+					elevio.SetMotorDirection(e.Dir)
+					e.Behaviour = ordStruct.E_Moving
+				}
+
+			case ordStruct.E_Moving:
+				e.Order[a.Button][a.Floor] = true
+				if a.Button == ordStruct.BT_Cab {
+					elevio.SetButtonLamp(ordStruct.BT_Cab, a.Floor, true)
+					break sel
+				}
+
+			case ordStruct.E_DoorOpen:
+				if e.Floor == a.Floor {
+					doorTimer = time.After(ordStruct.DOOR_OPEN_TIME)
+				} else {
+					e.Order[a.Button][a.Floor] = true
+					if a.Button == ordStruct.BT_Cab {
+						elevio.SetButtonLamp(ordStruct.BT_Cab, a.Floor, true)
+					}
+				}
+			}
+		case a := <-floorArrivals:
+			e.Floor = a
+			elevio.SetFloorIndicator(e.Floor)
+
+			switch e.Behaviour {
+			case ordStruct.E_Idle:
+				//do nothing
+			case ordStruct.E_Moving:
+				if orders.ShouldStop(e) {
+					elevio.SetMotorDirection(ordStruct.MD_Stop)
+					elevio.SetDoorOpenLamp(true)
+					e = orders.ClearOrdersAtCurrentFloor(e)
+					e = orders.ClearLightsAtCurrentFloor(e)
+					doorTimer = time.After(ordStruct.DOOR_OPEN_TIME)
+					e.Behaviour = ordStruct.E_DoorOpen
+				}
+			case ordStruct.E_DoorOpen:
+				//nothing
+			}
+			if e != prevElevator {
+				states <- e.Duplicate()
+			}
+		case <-doorTimer:
+
+			switch e.Behaviour {
+			case ordStruct.E_Idle:
+				fallthrough
+			case ordStruct.E_Moving:
+				fallthrough
+			case ordStruct.E_DoorOpen:
+				e.Dir = orders.ChooseDirection(e)
+				elevio.SetDoorOpenLamp(false)
+				elevio.SetMotorDirection(e.Dir)
+				if e.Dir == ordStruct.MD_Stop {
+					e.Behaviour = ordStruct.E_Idle
+				} else {
+					e.Behaviour = ordStruct.E_Moving
+				}
+			}
+			if prevElevator != e {
+				states <- e.Duplicate()
+			}
+		case a := <-updateLights:
+			e.LightMatrix = a
+			orders.UpdateLights(e)
+
+		}
+	}
+}
+
+func connectionMsgHandler(receive <-chan string, msgChan chan<- string,
+	newOrders chan ordStruct.ButtonEvent, states chan ordStruct.Elevator, updateLights chan<- ordStruct.LightType) {
+	msgToHandler := decoding.ElevatorMsg{Number: 1}
+	for {
+		select {
+		case a := <-receive:
+			msg := decoding.DecodeElevatorMsg(a)
+			buttons, floors := msgToHandler.E.Differences(msg.E)
+			for i, _ := range buttons {
+				newOrders <- ordStruct.ButtonEvent{
+					Floor:  floors[i],
+					Button: ordStruct.ButtonType(buttons[i]),
+				}
+			}
+			if msg.Number > msgToHandler.Number {
+				msgToHandler.Number = msg.Number
+			}
+			updateLights <- msg.E.LightMatrix
+		case e := <-states:
+			msgToHandler.E = e
+			msgToHandler.Number++
+			msgChan <- decoding.EncodeElevatorMsg(msgToHandler)
+		}
+	}
+}
---
+++ msgHandler2.go
@@ -0,0 +1,173 @@
+package main
+
+import (
+	"./MakkerModul/connector"
+	"./MakkerModul/decoding"
+	"./network/bcast"
+	"./network/idGenerator"
+	"./network/peers"
+	"./orders/elevio/ordStruct"
+	"flag"
+	"fmt"
+	"strings"
+	"time"
+)
+
+type relationship int
+
+const (
+	Crashed      relationship = 2
+	Connected                 = 1
+	Disconnected              = 0
+)
+
+type MsgHandler struct {
+	MyElevStates ordStruct.Elevator
+	MsgToElev    decoding.ElevatorMsg
+	MsgFromElev  decoding.ElevatorMsg
+
+	MsgToMaster   MsgFromHandlerToHandler
+	MsgFromMaster MsgFromHandlerToHandler
+
+	RelationElevator relationship
+	RelationMaster   relationship
+}
+
+type MsgFromHandlerToHandler struct {
+	Id     string
+	ElevId string
+	States ordStruct.Elevator
+	Number int
+}
+
+func main() {
+	H := MsgHandler{MsgFromElev: decoding.ElevatorMsg{Number: -1},
+		RelationElevator: Disconnected, RelationMaster: Disconnected}
+	pendingUpdates := make(chan string)
+	var myName string
+
+	flag.StringVar(&myName, "name", "", "id of this peer")
+	flag.Parse()
+
+	if myName == "" {
+		myName = idGenerator.GetRandomID()
+	}
+
+	id := "Elev - " + myName
+	fmt.Printf("#####################################\nelevtorMSGHANDLER with myName %v \n#####################################\n", myName)
+
+	H.MsgToMaster.Id = id
+	H.MsgToMaster.Number = 0
+
+	peerUpdateCh := make(chan peers.PeerUpdate)
+	peerTxEnable := make(chan bool)
+
+	go peers.Transmitter(15647, id, peerTxEnable, peerTxEnable)
+	go peers.Receiver(15647, peerUpdateCh)
+
+	netTx := make(chan MsgFromHandlerToHandler)
+	netRx := make(chan MsgFromHandlerToHandler)
+	repeatTx := make(chan MsgFromHandlerToHandler)
+	go bcast.Transmitter(16569, netTx)
+	go bcast.Receiver(16569, netRx)
+
+	go func() {
+		for a := range repeatTx {
+			for i := 0; i < 5; i++ {
+				netTx <- a
+				fmt.Println(a.Number)
+			}
+		}
+	}()
+
+	receiveLocal, msgChanLocal := connector.EstablishLocalTunnel(
+		"elevator_fsm2.go", 55556, 44445)
+	printStatus := make(chan bool)
+	go func() {
+		for {
+			time.Sleep(time.Second)
+			printStatus <- true
+		}
+	}()
+
+	go func() {
+		for update := range pendingUpdates {
+			switch update {
+			case "From myself":
+				msgChanLocal <- decoding.EncodeElevatorMsg(H.MsgToElev)
+			case "From MASTER":
+				msgChanLocal <- decoding.EncodeElevatorMsg(decoding.ElevatorMsg{E: H.MsgFromMaster.States})
+			case "To MASTER":
+				repeatTx <- H.MsgToMaster
+			}
+		}
+	}()
+
+	for {
+		select {
+		case p := <-peerUpdateCh:
+			for _, name := range p.Lost {
+				if strings.HasPrefix(name,"Backup"){ //"MASTER" {
+					fmt.Println("LOST CONNECTION TO MASTER")
+					H.RelationMaster = Disconnected
+				}
+			}
+			if strings.HasPrefix(p.New, "Backup") {
+				fmt.Printf("Connected to master \n")
+				H.RelationMaster = Connected
+			}
+
+		case a := <-netRx:
+			if strings.HasPrefix(a.Id, "Backup") {
+				fmt.Printf("Received from (not local) %v\n", a.Id)
+				if a.ElevId == id {
+					a.States.Order[0] = a.States.LightMatrix[0]
+					a.States.Order[1] = a.States.LightMatrix[1]
+				}
+				H.MsgFromMaster.States = a.States
+				pendingUpdates <- "From MASTER"
+			}
+
+		case a := <-receiveLocal:
+			switch a {
+			case "Connection lost":
+				H.MsgToElev = H.MsgFromElev
+				H.RelationElevator = Crashed
+			case "Connection established":
+				if H.RelationElevator == Disconnected {
+					H.RelationElevator = Connected
+					pendingUpdates <- "From myself"
+				}
+			default:
+				msg := decoding.DecodeElevatorMsg(a)
+				if msg.Number >= H.MsgFromElev.Number { // > ikke >= etter testing??
+					H.MsgFromElev = msg
+					if H.RelationMaster != Disconnected {
+						H.MsgToMaster.States = H.MsgFromElev.E
+						H.MsgToMaster.Number++
+						pendingUpdates <- "To MASTER"
+					} else {
+						H.MsgToElev = H.MsgFromElev
+						H.MsgToElev.E.Order[0] = H.MsgToElev.E.LightMatrix[0]
+						H.MsgToElev.E.Order[1] = H.MsgToElev.E.LightMatrix[1]
+						pendingUpdates <- "From myself"
+					}
+					H.MyElevStates = msg.E
+				} else {
+					H.RelationElevator = Crashed
+					pendingUpdates <- "From myself"
+				}
+				H.RelationElevator = Connected
+			}
+		case <-printStatus:
+			//fmt.Printf("\nPRINTOUT %v\n\n", H.MyElevStates)
+			/*default:
+			if H.RelationElevator == Crashed {
+				msgChanLocal <- decoding.EncodeMsg(H.MsgToElev)
+				H.RelationElevator = PendingUpdates
+			}
+			//case msg from master
+			*/
+		}
+	}
+}
