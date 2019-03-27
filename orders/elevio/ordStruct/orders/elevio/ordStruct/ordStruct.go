a4db3443f1b84ef4287b79c7a90d8bfdd9b155be
olavsbv <36269275+olavsbv@users.noreply.github.com>
2019-03-27 22:07:02 +0100

    Add files via upload
    Uæ, nå må vi starte på nytt

---
+++ orders/elevio/ordStruct/orders/elevio/ordStruct/ordStruct.go
@@ -0,0 +1,143 @@
+package ordStruct
+
+import "fmt"
+import "time"
+
+const DOOR_OPEN_TIME = 3 * time.Second
+
+type behaviourType int
+
+const (
+	E_Moving   behaviourType = 2
+	E_Idle                   = 1
+	E_DoorOpen               = 0
+)
+
+type MotorDirection int
+
+const (
+	MD_Up   MotorDirection = 1
+	MD_Down                = -1
+	MD_Stop                = 0
+)
+
+type ButtonType int
+
+const (
+	BT_HallUp   ButtonType = 0
+	BT_HallDown            = 1
+	BT_Cab                 = 2
+)
+
+type OrderType int
+
+const (
+	OT_Up   OrderType = 1
+	OT_Down           = -1
+	OT_Cab            = 0
+)
+
+type ButtonEvent struct {
+	Floor     int
+	Direction int
+	Button    ButtonType
+}
+
+type LightType [2][]bool
+
+type Elevator struct {
+	Floor       int
+	NumFloors   int
+	Dir         MotorDirection
+	Order       [3][]bool
+	Behaviour   behaviourType
+	LightMatrix LightType
+}
+
+func ElevatorInit(floors int) (e Elevator) {
+	e = Elevator{NumFloors: floors, Dir: 0}
+	for i := 0; i < 3; i++ {
+		for j := 1; j < floors; j++ {
+			e.Order[i] = append(e.Order[i], false)
+		}
+	}
+	for i := 0; i < 2; i++ {
+		for j := 1; j < floors; j++ {
+			e.LightMatrix[i] = append(e.LightMatrix[i], false)
+		}
+	}
+
+	return
+}
+func (e *Elevator) Duplicate() (e2 Elevator) {
+	e2 = *e
+	return
+}
+
+func (e *Elevator) AddOrder(button int, floor int) {
+	e.Order[button][floor] = true
+}
+
+func (e *Elevator) RemoveOrder(button ButtonType, floor int) {
+	e.Order[int(button)][floor] = false
+}
+
+func (e *Elevator) Differences(e2 Elevator) ([]int, []int) {
+	buttons := make([]int, 0)
+	floors := make([]int, 0)
+	for i := 0; i < 3; i++ {
+		for j := 0; j < e.NumFloors; j++ {
+			if e2.Order[i][j] == true && e.Order[i][j] == false {
+				buttons = append(buttons, i)
+				floors = append(floors, j)
+			}
+		}
+	}
+	return buttons, floors
+}
+
+func (e *Elevator) ClearOrders() {
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 4; j++ {
+			e.Order[i][j] = false
+		}
+	}
+}
+
+func (e *Elevator) PrintOrders() {
+	fmt.Print("[ ")
+	for i := 0; i < 3; i++ {
+		fmt.Println()
+		for j := 0; j < 4; j++ {
+			fmt.Print(e.Order[i][j], " ")
+		}
+	}
+	fmt.Print("]\n")
+}
+func (e *Elevator) PrintLightMatrix() {
+	fmt.Print("[ ")
+	for i := 0; i < 2; i++ {
+		fmt.Println()
+		for j := 0; j < 4; j++ {
+			fmt.Print(e.LightMatrix[i][j], " ")
+		}
+	}
+	fmt.Print("]\n")
+}
+
+/*
+func (e *Elevator) LightUpdate(button ButtonType,floor int, on bool, receiverLights chan<- LightEvent){
+	if button == BT_Cab {
+		fmt.Println("Error: external light can not be CAB!")
+	}
+	receiverLights<-LightEvent{Floor: floor, On: on, Button: button}
+
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 4; j++ {
+				e.Lights[i][j]|= externalList[i][j]
+			}
+		}
+	}
+
+}
+*/
